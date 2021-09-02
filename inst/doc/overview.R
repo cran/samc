dae9759cc6b90c9c351e232d9e60d15da2a20e3d
Andrew Marx <andrewjmarx@ufl.edu>
2021-09-02 13:30:02 +0000

    version 1.4.1

--- inst/doc/overview.R
+++ inst/doc/overview.R
@@ -14,17 +14,17 @@ library("samc")
 library("viridisLite")
 
 ## ---- fig.show='hold'---------------------------------------------------------
-str(samc::ex_res_data)
-str(samc::ex_abs_data)
-str(samc::ex_occ_data)
-
-
-plot(raster(samc::ex_res_data, xmn = 1, xmx = ncol(samc::ex_res_data), ymn = 1, ymx = nrow(samc::ex_res_data)),
-     main = "Example Resistance Data", xlab = "x", ylab = "y", col = viridis(256))
-
-plot(raster(samc::ex_abs_data, xmn = 1, xmx = ncol(samc::ex_abs_data), ymn = 1, ymx = nrow(samc::ex_abs_data)),
-     main = "Example Absorption Data", xlab = "x", ylab = "y", col = viridis(256))
-
-plot(raster(samc::ex_occ_data, xmn = 1, xmx = ncol(samc::ex_occ_data), ymn = 1, ymx = nrow(samc::ex_occ_data)),
-     main = "Example Occupancy Data", xlab = "x", ylab = "y", col = viridis(256))
+#  str(samc::ex_res_data)
+#  str(samc::ex_abs_data)
+#  str(samc::ex_occ_data)
+#  
+#  
+#  plot(raster(samc::ex_res_data, xmn = 1, xmx = ncol(samc::ex_res_data), ymn = 1, ymx = nrow(samc::ex_res_data)),
+#       main = "Example Resistance Data", xlab = "x", ylab = "y", col = viridis(256))
+#  
+#  plot(raster(samc::ex_abs_data, xmn = 1, xmx = ncol(samc::ex_abs_data), ymn = 1, ymx = nrow(samc::ex_abs_data)),
+#       main = "Example Absorption Data", xlab = "x", ylab = "y", col = viridis(256))
+#  
+#  plot(raster(samc::ex_occ_data, xmn = 1, xmx = ncol(samc::ex_occ_data), ymn = 1, ymx = nrow(samc::ex_occ_data)),
+#       main = "Example Occupancy Data", xlab = "x", ylab = "y", col = viridis(256))
 
