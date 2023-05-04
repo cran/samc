d5eefcee7d7ebb125b1237f8840ec7c81eeb8646
Andrew Marx <ajm.rpackages@gmail.com>
2023-05-04 05:50:02 +0000

    version 3.2.0

--- inst/doc/overview.R
+++ inst/doc/overview.R
@@ -14,13 +14,13 @@ library("samc")
 library("viridisLite")
 
 ## ---- fig.show='hold'---------------------------------------------------------
-#  str(samc::example_split_corridor)
-#  
-#  res_data <- samc::example_split_corridor$res
-#  abs_data <- samc::example_split_corridor$abs
-#  init_data <- samc::example_split_corridor$init
-#  
-#  plot(rasterize(res_data), main = "Example Resistance Data", xlab = "x", ylab = "y", col = viridis(256))
-#  plot(rasterize(abs_data), main = "Example Absorption Data", xlab = "x", ylab = "y", col = viridis(256))
-#  plot(rasterize(init_data), main = "Example Starting Location Data", xlab = "x", ylab = "y", col = viridis(256))
+str(samc::example_split_corridor)
+
+res_data <- samc::example_split_corridor$res
+abs_data <- samc::example_split_corridor$abs
+init_data <- samc::example_split_corridor$init
+
+plot(rasterize(res_data), main = "Example Resistance Data", xlab = "x", ylab = "y", col = viridis(256))
+plot(rasterize(abs_data), main = "Example Absorption Data", xlab = "x", ylab = "y", col = viridis(256))
+plot(rasterize(init_data), main = "Example Starting Location Data", xlab = "x", ylab = "y", col = viridis(256))
 
